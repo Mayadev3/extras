477c8982384be943132804d4e20557c2d3acf54b
Jim Rudolf <rudolf@jbrcodes.com>
2023-02-01 20:41:50 +0100

    Do main (without answers)

--- src/binary_tree.js
+++ src/binary_tree.js
@@ -31,35 +31,12 @@ class BinaryTree {
 
     // Return the node with the highest value in the tree
     findMaxValueNode() {
-        // It's surprisingly simple: Just follow right children for as
-        // long as we can; that's where the highest value is.
-        // (See the drawing in the test file.)
-        let node = this;
-        while (node.right) {
-            node = node.right;
-        }
-
-        return node;
     }
 
 
     // Return a count of the leaf nodes
     // (Note! It's recursive!)
     getLeafNodeCount() {
-        // Base case: We found a leaf node!
-        if (!this.left && !this.right) {
-            return 1;
-        }
-
-        let count = 0;
-        if (this.left) {
-            count += this.left.getLeafNodeCount();
-        }
-        if (this.right) {
-            count += this.right.getLeafNodeCount();
-        }
-
-        return count;
     }
 }
 
--- src/linked_list.js
+++ src/linked_list.js
@@ -19,7 +19,6 @@ class LinkedList {
     }
   
 
-    // Give this as a freebee... ;-)
     addToTail(val) {
         this.tail.next = new Node(val);
         this.tail = this.tail.next;
@@ -28,85 +27,26 @@ class LinkedList {
 
     // Return the sum of all the nodes' values
     getSum() {
-        let sum = 0;
-        let node = this.head;
-
-        while (node) {
-            sum += node.value;
-            node = node.next;
-        }
-
-        return sum;
     }
 
 
     // Return the values of the list as an array
     toArray() {
-        let arr = [];
-        let node = this.head;
-
-        while (node) {
-            arr.push(node.value);
-            node = node.next;
-        }
-
-        return arr;
     }
 
 
     // Return true if list2 is equal to this list
     isEqual(list2) {
-        let node1 = this.head;
-        let node2 = list2.head;
-
-        while (node1 && node2) {
-            if (node1.value !== node2.value) {
-                return false;
-            }
-
-            node1 = node1.next;
-            node2 = node2.next;
-        }
-
-        if (node1 || node2) {
-            return false;
-        }
-
-        return true;
     }
 
 
     // Return true if the list is a palindrome
     isPalindrome() {
-        let str = this.toArray().join('');
-        let revstr = str.split('').reverse().join('');
-
-        return str === revstr;
     }
 
     
     // Sort the list without creating new nodes
     sort() {
-        // Create array with all Node objs
-        let objs = [];
-        let node = this.head;
-        while (node) {
-            objs.push(node);
-            node = node.next;
-        }
-
-        // Now we can easily sort the Node objs by value
-        objs.sort((a, b) => a.value - b.value);
-
-        // Update .next pointers to point to next obj in array
-        for (let i=0; i<objs.length-1; i++) {
-            objs[i].next = objs[i+1];
-        }
-        objs[objs.length-1].next = null;
-
-        // Update head/tail pointers
-        this.head = objs[0];
-        this.tail = objs[objs.length-1];
     }
 }  
 
